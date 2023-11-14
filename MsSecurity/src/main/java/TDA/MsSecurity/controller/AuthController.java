ca6bc81786dcb17ce86ec4b0d0e55a13cb2e1b52
YersonTineo56 <yeileon1410@gmail.com>
2023-11-14 17:56:30 -0500

    Update AuthController.java
    se crearon los metodos, en torno a mi pc
    
    Co-Authored-By: Manuel <109305586+ManuelChiriBarbaran@users.noreply.github.com>
    Co-Authored-By: jhonatanGibtHub <100153097+jhonatanGibtHub@users.noreply.github.com>

--- MsSecurity/src/main/java/TDA/MsSecurity/controller/AuthController.java
+++ MsSecurity/src/main/java/TDA/MsSecurity/controller/AuthController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +30,38 @@ public class AuthController {
 
         
     }
-   
+    @GetMapping("/{id}")
+    public ResponseEntity<modelUsuario> getUserById(@PathVariable int id) {
+        modelUsuario usuario = authService.getUserById(id);
+        if (usuario != null) {
+            return new ResponseEntity<>(usuario, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<modelUsuario> createUser(@RequestBody modelUsuario nuevoUsuario) {
+        modelUsuario usuarioCreado = authService.createUser(nuevoUsuario);
+        return new ResponseEntity<>(usuarioCreado, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<modelUsuario> updateUser(@PathVariable int id, @RequestBody modelUsuario usuarioActualizado) {
+        modelUsuario usuarioActualizado = authService.updateUser(id, usuarioActualizado);
+
+        if (usuarioActualizado != null) {
+            return new ResponseEntity<>(usuarioActualizado, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable int id) {
+        authService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
 
