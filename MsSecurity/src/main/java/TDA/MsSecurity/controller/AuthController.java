429bb819fe53fc9e11c22766997685f1b2e36a06
YersonTineo56 <yeileon1410@gmail.com>
2023-11-14 00:17:35 -0500

    Update AuthController.java
    se creo la clase para buscar por id

--- MsSecurity/src/main/java/TDA/MsSecurity/controller/AuthController.java
+++ MsSecurity/src/main/java/TDA/MsSecurity/controller/AuthController.java
@@ -2,7 +2,10 @@ package TDA.MsSecurity.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,11 @@ public class AuthController {
     public List<modelUsuario> get() {
         return authService.getAllUsers();
     }   
+    @GetMapping("/{id}")
+    public modelUsuario getUserById(@PathVariable int id) {
+        return authService.getUserById(id);
+    }
 
+   
 }
+
