db30b76346ac2efaeb32c361fe5fb5ab44f83a43
YersonTineo56 <yeileon1410@gmail.com>
2023-12-17 00:35:08 -0500

    Update MsSecurityApplication.java

--- MsSecurity/src/main/java/TDA/MsSecurity/MsSecurityApplication.java
+++ MsSecurity/src/main/java/TDA/MsSecurity/MsSecurityApplication.java
@@ -3,6 +3,7 @@ package TDA.MsSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MsSecurityApplication {
 
