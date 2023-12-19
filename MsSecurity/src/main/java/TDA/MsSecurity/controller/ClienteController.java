90e34a302ee3e17256fdf73d150e1b7bc1bbace7
YersonTineo56 <yeileon1410@gmail.com>
2023-12-19 17:08:31 -0500

    Create ClienteController.java
    se creo cliente controller

---
+++ MsSecurity/src/main/java/TDA/MsSecurity/controller/ClienteController.java
@@ -0,0 +1,48 @@
+package TDA.MsSecurity.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public List<Cliente> obtenerClientes() {
+        return clienteService.obtenerTodosClientes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> obtenerClientePorId(@PathVariable Long id) {
+        Cliente cliente = clienteService.obtenerClientePorId(id);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> crearCliente(@RequestBody Cliente cliente) {
+        Cliente nuevoCliente = clienteService.crearCliente(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoCliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> actualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
+        Cliente clienteActualizado = clienteService.actualizarCliente(id, cliente);
+        return ResponseEntity.ok(clienteActualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarCliente(@PathVariable Long id) {
+        clienteService.eliminarCliente(id);
+        return ResponseEntity.noContent().build();
+    }
+}
