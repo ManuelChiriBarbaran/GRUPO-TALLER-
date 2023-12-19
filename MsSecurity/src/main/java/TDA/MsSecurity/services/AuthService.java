0e9d9c9eb53c622fc915d5abb7e422b52004d996
YersonTineo56 <yeileon1410@gmail.com>
2023-12-19 13:34:22 -0500

    Update AuthService.java
    mas cambios xd

--- MsSecurity/src/main/java/TDA/MsSecurity/services/AuthService.java
+++ MsSecurity/src/main/java/TDA/MsSecurity/services/AuthService.java
@@ -17,8 +17,31 @@ public class AuthService {
     public List<modelUsuario> getAllUsers() {
         return (List<modelUsuario>) authRepository.findAll();
     }
-    
-  
+    public modelUsuario getUserById(int id) {
+        return authRepository.findById(id).orElse(null);
     }
 
+    public modelUsuario createUser(modelUsuario nuevoUsuario) {
+        return authRepository.save(nuevoUsuario);
+    }
+
+    public modelUsuario updateUser(int id, modelUsuario usuarioActualizado) {
+        modelUsuario usuarioExistente = authRepository.findById(id).orElse(null);
+
+        if (usuarioExistente != null) {
+            usuarioExistente.setUsuario(usuarioActualizado.getUsuario());
+            usuarioExistente.setClave(usuarioActualizado.getClave());
+            return authRepository.save(usuarioExistente);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteUser(int id) {
+        // Elimina el usuario con el ID proporcionado, si existe
+        authRepository.deleteById(id);
+    }
+
+  
+}
 
