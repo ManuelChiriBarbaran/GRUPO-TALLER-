7628c2d5781f4949cb0d22487166dc1ec93e4420
YersonTineo56 <yeileon1410@gmail.com>
2023-12-19 17:09:48 -0500

    Create HabitacionRepository.java
    se creo habitacionRepository
    
    Co-Authored-By: Manuel <109305586+ManuelChiriBarbaran@users.noreply.github.com>
    Co-Authored-By: jhonatanGibtHub <100153097+jhonatanGibtHub@users.noreply.github.com>

---
+++ MsSecurity/src/main/java/TDA/MsSecurity/repository/HabitacionRepository.java
@@ -0,0 +1,6 @@
+package TDA.MsSecurity.repository;
+
+@Repository
+public interface HabitacionRepository extends JpaRepository<Habitacion, Long> {
+    // Puedes agregar métodos personalizados según tus necesidades
+}
