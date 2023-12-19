91b6238c8622d067728e60ffa9f96d8aace6a5a7
YersonTineo56 <yeileon1410@gmail.com>
2023-12-19 17:09:32 -0500

    Create ClienteRepository.java
    se creo clienteRepository
    
    Co-Authored-By: Manuel <109305586+ManuelChiriBarbaran@users.noreply.github.com>
    Co-Authored-By: jhonatanGibtHub <100153097+jhonatanGibtHub@users.noreply.github.com>

---
+++ MsSecurity/src/main/java/TDA/MsSecurity/repository/ClienteRepository.java
@@ -0,0 +1,5 @@
+package TDA.MsSecurity.repository;
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    // Puedes agregar métodos personalizados según tus necesidades
+}
