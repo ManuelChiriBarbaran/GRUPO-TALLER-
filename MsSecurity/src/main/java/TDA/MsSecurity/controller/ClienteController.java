8219338d4f463ae852deedacfaee949e03c667ee
YersonTineo56 <>
2023-12-21 09:17:04 -0500

    Update ClienteController.java
    Se importaron

--- MsSecurity/src/main/java/TDA/MsSecurity/controller/ClienteController.java
+++ MsSecurity/src/main/java/TDA/MsSecurity/controller/ClienteController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import TDA.MsSecurity.model.modelUsuario;
+import TDA.MsSecurity.services.AuthService;
+
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
