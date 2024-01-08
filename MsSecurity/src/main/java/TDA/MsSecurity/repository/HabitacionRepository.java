fef3db17481f46641a3e18a9572a76fcc201e6e8
YersonTineo56 <yeileon1410@gmail.com>
2024-01-08 11:34:16 -0500

    Update HabitacionRepository.java

--- MsSecurity/src/main/java/TDA/MsSecurity/repository/HabitacionRepository.java
+++ MsSecurity/src/main/java/TDA/MsSecurity/repository/HabitacionRepository.java
@@ -1,6 +1,11 @@
 package TDA.MsSecurity.repository;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import TDA.MsSecurity.model.Habitacion;
 @Repository
-public interface HabitacionRepository extends JpaRepository<Habitacion, Long> {
-    // Puedes agregar métodos personalizados según tus necesidades
+
+public interface HabitacionRepository extends CrudRepository<Habitacion, Long> {
+    // No se requiere código adicional, ya que CrudRepository proporciona métodos CRUD básicos
 }
