1c43d9250d9829d1aa613dc08c10ee2ffa287a3d
YersonTineo56 <yeileon1410@gmail.com>
2024-01-08 11:34:20 -0500

    Update ClienteService.java

--- MsSecurity/src/main/java/TDA/MsSecurity/services/ClienteService.java
+++ MsSecurity/src/main/java/TDA/MsSecurity/services/ClienteService.java
@@ -1,40 +1,47 @@
 package TDA.MsSecurity.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import TDA.MsSecurity.model.Cliente;
 import TDA.MsSecurity.repository.ClienteRepository;
+
+
+// En el paquete service
 @Service
 public class ClienteService {
     @Autowired
     private ClienteRepository clienteRepository;
 
     public List<Cliente> obtenerTodosClientes() {
-        return clienteRepository.findAll();
+        return (List<Cliente>) clienteRepository.findAll();
     }
 
     public Cliente obtenerClientePorId(Long id) {
         return clienteRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Cliente no encontrado con id: " + id));
+                .orElseThrow();
     }
 
-    public Cliente crearCliente(Cliente cliente) {
-        return clienteRepository.save(cliente);
+    public Cliente crearCliente(Cliente nuevoCliente) {
+        return clienteRepository.save(nuevoCliente);
     }
 
-    public Cliente actualizarCliente(Long id, Cliente cliente) {
-        Cliente clienteExistente = obtenerClientePorId(id);
-        // Actualiza los atributos según sea necesario
-        clienteExistente.setNombre(cliente.getNombre());
-        clienteExistente.setApellido(cliente.getApellido());
-        clienteExistente.setDireccion(cliente.getDireccion());
-        clienteExistente.setTelefono(cliente.getTelefono());
-        clienteExistente.setCorreoElectronico(cliente.getCorreoElectronico());
+    public Cliente actualizarCliente(Long id, Cliente clienteActualizado) {
+        Cliente clienteExistente = clienteRepository.findById(id)
+                .orElseThrow();
+
+        clienteExistente.setNombre(clienteActualizado.getNombre());
+        clienteExistente.setApellido(clienteActualizado.getApellido());
+        clienteExistente.setDireccion(clienteActualizado.getDireccion());
+        clienteExistente.setTelefono(clienteActualizado.getTelefono());
+        clienteExistente.setCorreoElectronico(clienteActualizado.getCorreoElectronico());
+
         return clienteRepository.save(clienteExistente);
     }
 
     public void eliminarCliente(Long id) {
-        Cliente cliente = obtenerClientePorId(id);
-        clienteRepository.delete(cliente);
+        clienteRepository.deleteById(id);
     }
 }
