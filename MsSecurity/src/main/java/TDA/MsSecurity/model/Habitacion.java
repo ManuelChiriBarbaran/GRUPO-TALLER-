4a7f7ee98781e8f0e7aaeef61f7af90183013f21
YersonTineo56 <jhonatancito0015@gmail.com>
2024-01-08 11:34:05 -0500

    Update Habitacion.java

--- MsSecurity/src/main/java/TDA/MsSecurity/model/Habitacion.java
+++ MsSecurity/src/main/java/TDA/MsSecurity/model/Habitacion.java
@@ -1,4 +1,9 @@
 package TDA.MsSecurity.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "habitacion")
@@ -6,10 +11,42 @@ public class Habitacion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    public Integer getNumeroHabitacion() {
+        return numeroHabitacion;
+    }
+    public void setNumeroHabitacion(Integer numeroHabitacion) {
+        this.numeroHabitacion = numeroHabitacion;
+    }
     private Integer numeroHabitacion;
+    public String getTipoHabitacion() {
+        return tipoHabitacion;
+    }
+    public void setTipoHabitacion(String tipoHabitacion) {
+        this.tipoHabitacion = tipoHabitacion;
+    }
     private String tipoHabitacion;
+    public Double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
     private Double precio;
+    public Habitacion(Long id, Integer numeroHabitacion, String tipoHabitacion, Double precio, String estado) {
+        this.id = id;
+        this.numeroHabitacion = numeroHabitacion;
+        this.tipoHabitacion = tipoHabitacion;
+        this.precio = precio;
+        this.estado = estado;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
     private String estado;
 
     // Constructores, getters y setters
+
 }
