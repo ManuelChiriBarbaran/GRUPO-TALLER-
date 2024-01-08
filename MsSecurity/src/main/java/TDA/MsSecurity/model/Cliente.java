07de76670f76f4a06d533bae9e0daf53cfdaa630
YersonTineo56 <jhonatancito0015@gmail.com>
2024-01-08 11:34:03 -0500

    Update Cliente.java

--- MsSecurity/src/main/java/TDA/MsSecurity/model/Cliente.java
+++ MsSecurity/src/main/java/TDA/MsSecurity/model/Cliente.java
@@ -1,16 +1,81 @@
 package TDA.MsSecurity.model;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 @Entity
 @Table(name = "cliente")
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
     private String nombre;
     private String apellido;
+    public String getApellido() {
+        return apellido;
+    }
+
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+
     private String direccion;
+    public String getTelefono() {
+        return telefono;
+    }
+
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+
     private String telefono;
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+
     private String correoElectronico;
+
+
+    public Cliente(Long id, String nombre, String apellido, String direccion, String telefono,
+            String correoElectronico) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+    }
 
     // Constructores, getters y setters
 
